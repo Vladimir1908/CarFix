27ab72a76f0cc8636c4c9c0317c87fe1ede56701
Vladimir1908 <vladimir.virlan@isa.utm.md>
2026-04-28 20:18:39 +0300

    feat: restructure backend architecture and project organization

--- backend/CarFix.API/Program.cs
+++ backend/CarFix.API/Program.cs
@@ -1,33 +1,20 @@
-using CarFix.BusinessLayer.Interfaces;
-using CarFix.BusinessLayer.Services;
-using CarFix.DataAccess;
-using CarFix.DataAccess.Repositories;
-using CarFix.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
+
+CarFix.DataAccess.DbSession.ConnectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<CarFixDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IGenericRepository<Client>, GenericRepository<Client>>();
-builder.Services.AddScoped<IGenericRepository<Service>, GenericRepository<Service>>();
-builder.Services.AddScoped<IGenericRepository<Appointment>, GenericRepository<Appointment>>();
-
-builder.Services.AddScoped<IClientService, ClientService>();
-builder.Services.AddScoped<IServiceService, ServiceService>();
-builder.Services.AddScoped<IAppointmentService, AppointmentService>();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173")
+    {
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod());
+              .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
@@ -41,11 +28,4 @@ if (app.Environment.IsDevelopment())
 app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<CarFixDbContext>();
-    db.Database.Migrate();
-}
-
 app.Run();
