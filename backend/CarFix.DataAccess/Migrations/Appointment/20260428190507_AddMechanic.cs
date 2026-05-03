96acba9f8e3f28d72329c5bd41fc80ad332dc251
nicolaev91 <nicolaev.adrian23@gmail.com>
2026-05-03 16:41:33 +0300

    feat: add migration AddMechanic to Appointments table

---
+++ backend/CarFix.DataAccess/Migrations/Appointment/20260428190507_AddMechanic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarFix.DataAccess.Migrations.Appointment
+{
+    /// <inheritdoc />
+    public partial class AddMechanic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mechanic",
+                table: "Appointments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mechanic",
+                table: "Appointments");
+        }
+    }
+}
